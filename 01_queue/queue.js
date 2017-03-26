49e003386103a59f151dcfffc8576961a7437f52
Henry Bao-Viet Nguyen <hbn@Henrys-MacBook-Pro.local>
2017-03-26 16:33:24 -0400

    stacks and queues

--- 01_queue/queue.js
+++ 01_queue/queue.js
@@ -1,3 +1,18 @@
 function Queue () {
-	
+	this.array = [];
 }
+
+Queue.prototype.size = function () {
+	return this.array.length;
+};
+
+Queue.prototype.enqueue = function (item) {
+	this.array.push(item);
+	return this.array;
+};
+
+Queue.prototype.dequeue = function () {
+	this.array.pop();
+	return this.array;
+};
+
